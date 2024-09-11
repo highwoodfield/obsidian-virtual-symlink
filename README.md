f407ccb3615e5e707242ab8022216b7931ec8b31
highwoodfield <178282378+highwoodfield@users.noreply.github.com>
2024-09-11 19:11:07 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Obsidian Virtual Symlink
+
+This plugin is made for Obsidian.
+By installing it, you can create a note that contains a link to the currently opened note.
+
+このプラグインはObsidianのためのプラグインです。
+このプラグインを導入することで、開いているノートへのリンクが記載された新しいノートを作成することができます。
+
+## Motivation
+
+I don't categorize notes using tags but rather folders. Therefore, I had to figure out how to categorize a note into multiple categories. I decided to create a note that contains a link to the currently opened note and place it in another folder. To automate this process, I developed this plugin.
+
+私はノートを、タグでなくフォルダによって分類しています。
+そのため、ある１つのノートを複数のフォルダに分類するためには工夫が必要です。
+私は、そのノートへのリンクを記載したノートを手動で作成し、
+元のノートの分類先とは別の分類先にそのノートを分類することにしました。
+このプラグインは、この作業を自動化するために作成されました。
---
+++ src/main.ts
@@ -0,0 +1,70 @@
+/** @format */
+
+import {App, MarkdownView, Modal, Notice, Plugin, Setting, TFile, Vault} from 'obsidian'
+
+interface VirtSymlinkSettings {
+}
+
+const DEFAULT_SETTINGS: VirtSymlinkSettings = {}
+
+export default class VirtSymlinkPlugin extends Plugin {
+    settings: VirtSymlinkSettings
+
+    async onload() {
+        // await this.loadSettings();
+
+		this.addCommand({
+			id: "virtsymlink-create-virtual-symlink",
+			name: "Create a virtual symlink",
+
+			checkCallback: (checking: boolean) => {
+				return this.insertCallback(checking, true);
+			}
+		});
+    }
+
+	insertCallback(checking: boolean, askDate: boolean): boolean | void {
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView)
+		if (activeView == null) return false;
+		const file = activeView.file
+		if (file == null) return false;
+		if (checking) return true;
+
+		this.createVirtualSymlink(file)
+			.then(() => {
+				new Notice("Virtual Symlink: Success")
+			})
+			.catch(e => {
+				const msg = `Virtual Symlink: Error (${e})`;
+				new Notice(msg);
+				console.error(msg);
+			});
+	}
+
+	async createVirtualSymlink(file: TFile): Promise<void> {
+		const content = `---
+virtual-symlink-target: ${file.path}
+---
+
+LNK [[${file.path}]]
+This note is created by Virtual Symlink plugin.
+`;
+		const parent = file.parent;
+		if (parent == null) {
+			throw new Error("parent == null");
+		}
+		await this.app.vault.create(`${parent.path}/LNK ${file.basename}.md`, content);
+	}
+
+    onunload() {
+        console.log('unloading plugin')
+    }
+
+    async loadSettings() {
+        this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData())
+    }
+
+    async saveSettings() {
+        await this.saveData(this.settings)
+    }
+}
